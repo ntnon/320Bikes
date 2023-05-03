3b9de784ac1991b9c72470bfd4a7ddbd0d423ecd
Anton-lr <Anton.nydal@hotmail.com>
2023-05-03 15:19:57 +0200

    exported csv's to sql's

---
+++ DB/blue_2.sql
@@ -0,0 +1,12 @@
+CREATE TABLE blue_2(
+   last_seen       VARCHAR(25) NOT NULL PRIMARY KEY
+  ,id              INTEGER  NOT NULL
+  ,name            VARCHAR(21) NOT NULL
+  ,bikes_in_use    INTEGER  NOT NULL
+  ,bikes_available INTEGER  NOT NULL
+  ,longitude       NUMERIC(14,12) NOT NULL
+  ,latitude        NUMERIC(15,12) NOT NULL
+  ,geopoint        VARCHAR(19) NOT NULL
+  ,type            BIT  NOT NULL
+);
+INSERT INTO blue_2(last_seen,id,name,bikes_in_use,bikes_available,longitude,latitude,geopoint,type) VALUES ('2023-05-02T12:29:06+00:00',133,'Station Gent-St. P. (M. Hendrikaplein)',55,4,3.709667000000,51.034889000000,'51.034889, 3.709667',1);
---
+++ DB/data_overview.sql
@@ -0,0 +1,17 @@
+CREATE TABLE data_overview(
+   full_name  VARCHAR(55) NOT NULL PRIMARY KEY
+  ,short_name VARCHAR(12) NOT NULL
+  ,type       VARCHAR(5) NOT NULL
+  ,link       VARCHAR(101) NOT NULL
+);
+INSERT INTO data_overview(full_name,short_name,type,link) VALUES ('baqme-deelfietsen-beschikbaarheid-station-gent','baqme_1','hub','https://data.stad.gent/explore/dataset/baqme-deelfietsen-beschikbaarheid-station-gent/table/');
+INSERT INTO data_overview(full_name,short_name,type,link) VALUES ('baqme-deelfietsen-stations-locaties-gent','baqme_2','hub','https://data.stad.gent/explore/dataset/baqme-deelfietsen-stations-locaties-gent/table/');
+INSERT INTO data_overview(full_name,short_name,type,link) VALUES ('baqme-locaties-vrije-deelfietsen-gent','baqme_3','hub','https://data.stad.gent/explore/dataset/baqme-locaties-vrije-deelfietsen-gent/table/');
+INSERT INTO data_overview(full_name,short_name,type,link) VALUES ('blue-bike-deelfietsen-gent-dampoort','blue_1','hub','https://data.stad.gent/explore/dataset/blue-bike-deelfietsen-gent-dampoort/table/');
+INSERT INTO data_overview(full_name,short_name,type,link) VALUES ('blue-bike-deelfietsen-gent-sint-pieters-m-hendrikaplein','blue_2','hub','https://data.stad.gent/explore/dataset/blue-bike-deelfietsen-gent-sint-pieters-m-hendrikaplein/table/');
+INSERT INTO data_overview(full_name,short_name,type,link) VALUES ('blue-bike-deelfietsen-gent-sint-pieters-st-denijslaan','blue_3','hub','https://data.stad.gent/explore/dataset/blue-bike-deelfietsen-gent-sint-pieters-st-denijslaan/table/');
+INSERT INTO data_overview(full_name,short_name,type,link) VALUES ('blue-bike-deelfietsen-merelbeke-drongen-wondelgem','blue_4','hub','https://data.stad.gent/explore/dataset/blue-bike-deelfietsen-merelbeke-drongen-wondelgem/table/');
+INSERT INTO data_overview(full_name,short_name,type,link) VALUES ('donkey-republic-deelfietsen-stations-locaties','donkey_1','hub','https://data.stad.gent/explore/dataset/donkey-republic-beschikbaarheid-deelfietsen-per-station/table/');
+INSERT INTO data_overview(full_name,short_name,type,link) VALUES ('dott-deelfietsen-hubs-gent','dott_1','hub','https://data.stad.gent/explore/dataset/dott-deelfietsen-gent/table/');
+INSERT INTO data_overview(full_name,short_name,type,link) VALUES ('donkey-republic-beschikbaarheid-deelfietsen-per-station','data_overview','bikes','https://data.stad.gent/explore/dataset/donkey-republic-beschikbaarheid-deelfietsen-per-station/table/');
+INSERT INTO data_overview(full_name,short_name,type,link) VALUES ('dott-deelfietsen-gent','dott_bikes','bikes','https://data.stad.gent/explore/dataset/dott-deelfietsen-gent/table/');
