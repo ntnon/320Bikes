df8c1b1369452d0be2bcd07ffaa8d92db20b9e98
Anton-lr <Anton.nydal@hotmail.com>
2023-05-09 16:22:16 +0200

    added sql

---
+++ DB/blue_3.sql
@@ -0,0 +1,12 @@
+CREATE TABLE blue_3(
+   last_seen       VARCHAR(25) NOT NULL PRIMARY KEY
+  ,id              INTEGER  NOT NULL
+  ,name            VARCHAR(50) NOT NULL
+  ,bikes_in_use    INTEGER  NOT NULL
+  ,bikes_available INTEGER  NOT NULL
+  ,longitude       NUMERIC(14,12) NOT NULL
+  ,latitude        NUMERIC(15,12) NOT NULL
+  ,geopoint        VARCHAR(19) NOT NULL
+  ,type            BIT  NOT NULL
+);
+INSERT INTO blue_3(last_seen,id,name,bikes_in_use,bikes_available,longitude,latitude,geopoint,type) VALUES ('2023-05-02T17:49:44+02:00',73,'Station Gent-St. P. (St.-Denijslaan)',52,16,3.709667000000,51.034889000000,'51.034889, 3.709667',1);
